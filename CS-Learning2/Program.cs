4ceafed9f84d7afc0d90c17d96a4ccd2e49e1d8d
igorlech <eaaigle@students.eaaa.dk>
2023-12-23 14:07:22 +0100

    updated console messages

--- CS-Learning2/Program.cs
+++ CS-Learning2/Program.cs
@@ -17,12 +17,12 @@
             Console.WriteLine("Enter first number");
 
             num1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(num1);
+            Console.WriteLine("Chosen number:" + num1);
 
             Console.WriteLine("Enter second number");
 
             num2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(num2);
+            Console.WriteLine("Chosen number:" + num2);
 
             Console.WriteLine("Choose the type of the operation that you would like to perform. Press a for addition, s for substraction, m for multiplication and any other key for division");
 
